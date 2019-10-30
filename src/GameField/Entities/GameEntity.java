bba3f771b951c21b629d560478c28044f49340b5
miho <miho@users.noreply.github.com>
2019-10-30 10:13:39 +0700

    sua lai background

---
+++ src/GameField/Entities/GameEntity.java
@@ -0,0 +1,11 @@
+package GameField.Entities;
+
+public interface GameEntity {
+    double getPosX();
+    double getPosY();
+    double getWidth();
+    double getHeight();
+    //boolean isContaining(double posX, double posY, double width, double height);
+    //boolean isBeingContained(double posX, double posY, double width, double height);
+    //boolean isBeingOverlapped(double posX, double posY, double width, double height);
+}
