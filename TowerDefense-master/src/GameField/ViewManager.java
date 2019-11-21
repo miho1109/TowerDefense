6dec9a006fe7b37d3ca4b31c974547da280cdce1
ngdangha <56758025+ngdangha@users.noreply.github.com>
2019-11-21 22:48:56 +0700

    Tower Button  n Tower

---
+++ TowerDefense-master/src/GameField/ViewManager.java
@@ -0,0 +1,47 @@
+package GameField;
+
+import GameField.Entities.Enemy.NormalTroop;
+import GameField.Entities.GameEntity;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class ViewManager extends VBox{
+
+    private static final int HEIGHT = 1350;
+    private static final int WIDTH = 720;
+    public static AnchorPane mainPane;
+    private Scene mainScene;
+    private Stage mainStage;
+
+    public ViewManager(){
+        mainPane = new AnchorPane();
+        mainScene = new Scene(mainPane,HEIGHT, WIDTH);
+        mainStage = new Stage();
+        mainStage.setTitle("TowerDefense");
+        mainStage.setScene(mainScene);
+        createBackGround();
+        GameControl.drawGrid();
+        GameControl.spawnTroop();
+        GameControl.printMousePosition();
+        //GameControl.spawnTower();
+        GameControl.spawnTowerButton();
+        GameControl.mouseClicked();
+    }
+
+
+    public Stage getMainStage(){
+        return mainStage;
+    }
+
+    private void createBackGround(){
+        Image backgroundImage = new Image("GameField/resources/background2.png", HEIGHT, WIDTH, true, true);
+        BackgroundImage background = new BackgroundImage(backgroundImage, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, null);
+        mainPane.setBackground(new Background(background));
+    }
+}
