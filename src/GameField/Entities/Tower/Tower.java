63d498d877514e246930a8c240394b87def635da
ngdangha <56758025+ngdangha@users.noreply.github.com>
2019-11-28 23:54:51 +0700

    towerType

--- src/GameField/Entities/Tower/Tower.java
+++ src/GameField/Entities/Tower/Tower.java
@@ -1,24 +1,90 @@
 package GameField.Entities.Tower;
 
 import GameField.Entities.GameEntity;
+import GameField.Grid;
+import GameField.ViewManager;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import static GameField.ViewManager.mainPane;
 
 public class Tower implements GameEntity {
+    protected ImageView TowerImage;
+    public ObjectType currentType;
 
-    public Tower() {}
+    private double x;
+    private double y;
+    private double width;
+    private double height;
+    private double fireRate;
+    private double damage;
+    private boolean dragAble = true;
 
-    public void getTowerType(ObjectType type){
-        switch (type){
-            case greenTower:
-                new greenTower();
+    public Tower(ObjectType type) {
+        currentType = type;
+        dragTower();
+        loadTowerImage(type);
+        ViewManager.mainPane.getChildren().add(TowerImage);
+    }
+
+    public ObjectType getCurrentType() {
+        return currentType;
+    }
+
+    private void loadTowerImage(ObjectType type) {
+        switch (type) {
+            case normalTower:
+                loadImage("GameField/Entities/Tower/Resources/normalTower.png");
+                break;
+            /*
+            case airTower:
+                loadImage("GameField/Entities/Tower/Resources/airTower.png");
+                break;
+
+            case lightTower:
+                loadImage("GameField/Entities/Tower/Resources/lightTower.png");
+                break;
+
+            case heavyTower:
+                loadImage("GameField/Entities/Tower/Resources/heavyTower.png");
+                break;
+             */
         }
     }
 
-    public double getPosX(ObjectType type){
-        switch (type){
-            case greenTower:
-                new greenTower();
-        }
-        return 0;
+    private void loadImage(String location) {
+        TowerImage = new ImageView(new Image(location));
+    }
+
+    public void clearImage() {
+        ViewManager.mainPane.getChildren().remove(TowerImage);
+        TowerImage = null;
+    }
+
+    private boolean spawnAble(double x, double y) {
+        int i = (int) y / 90;
+        int j = (int) x / 90;
+        if (Grid.newGrid[i][j] == 1) return true;
+        else return false;
+    }
+
+    private void dragTower() {
+
+        mainPane.setOnMouseDragged(event -> {
+            if (dragAble) {
+                TowerImage.setX(event.getSceneX() - 45);
+                TowerImage.setY(event.getSceneY() - 50);
+            }
+        });
+
+        mainPane.setOnMouseReleased(event -> {
+            if ((spawnAble(event.getSceneX(), event.getSceneY())) && (dragAble)) {
+                TowerImage.setX(((int) (event.getSceneX() / 90) * 90));
+                TowerImage.setY(((int) (event.getSceneY() / 90) * 90));
+                dragAble = false;
+            }
+        });
+
     }
 
     public double getPosX() {
